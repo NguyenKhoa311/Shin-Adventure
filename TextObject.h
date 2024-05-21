ecbd4eebfbdbbdd92ec7d867fcaf5c4640bde0b0
Khoa Nguyen <160843987+NguyenKhoa311@users.noreply.github.com>
2024-05-21 21:31:37 +0700

    Update TextObject.h

--- TextObject.h
+++ TextObject.h
@@ -1,7 +1,3 @@
-//
-// Created by Hoang Nguyen on 30/04/2024.
-//
-
 #ifndef UNDEFINED_GAME_TEXTOBJECT_H
 #define UNDEFINED_GAME_TEXTOBJECT_H
 
@@ -20,13 +16,9 @@ public:
         YELLOW_TEXT = 3,
     };
     bool LoadFromRenderText(SDL_Renderer* renderer, TTF_Font* font);
-//    void SetColor(Uint8 red,Uint8 green, Uint8 blue);
     void SetColor(int type);
     void RenderText(SDL_Renderer* renderer, int x, int y, SDL_Rect* clip = nullptr, double angle = 0.0, SDL_Point* center = nullptr, SDL_RendererFlip flip = SDL_FLIP_NONE);
-//    int get_width() const {return width;}
-//    int get_height() const {return height;}
     void SetText(const std::string& text) {str_val = text;}
-//    std::string GetText() const {return str_val;}
     void Free();
 
 private:
