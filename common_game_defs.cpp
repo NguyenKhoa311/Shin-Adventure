3bb701789904148061576decebc7bf7d352379b7
Khoa Nguyen <160843987+NguyenKhoa311@users.noreply.github.com>
2024-05-21 21:33:34 +0700

    Update common_game_defs.cpp

--- common_game_defs.cpp
+++ common_game_defs.cpp
@@ -1,13 +1,13 @@
 #include "common_game_defs.h"
 
 bool GameDef::CheckCollision(const SDL_Rect &obj_a, const SDL_Rect &obj_b) {
-    // Calculate the sides of rect A
+    // get obj_a 
     int left_a = obj_a.x;
     int right_a = obj_a.x + obj_a.w;
     int top_a = obj_a.y;
     int bottom_a = obj_a.y + obj_a.h;
 
-    // Calculate the sides of rect B
+    // get obj_b
     int left_b = obj_b.x;
     int right_b = obj_b.x + obj_b.w;
     int top_b = obj_b.y;
