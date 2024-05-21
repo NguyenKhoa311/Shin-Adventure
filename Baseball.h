1a2c17cd202a69ea6884a48296968234364ce8c4
Khoa Nguyen <160843987+NguyenKhoa311@users.noreply.github.com>
2024-05-21 21:17:07 +0700

    Update Baseball.h

--- Baseball.h
+++ Baseball.h
@@ -1,7 +1,3 @@
-//
-// Created by Hoang Nguyen on 06/04/2024.
-//
-
 #ifndef BASEBALL_H
 #define BASEBALL_H
 #include "common_game_defs.h"
@@ -20,13 +16,9 @@ public:
     };
 
     void set_x_val(const int& xVelo) {x_velo = xVelo;}
-//    [[nodiscard]] int get_x_velo() const {return x_speed;}
-
     void set_is_move(const bool& isMove) {is_move = isMove;}
     [[nodiscard]] bool get_is_move() const {return is_move;}
-
     void set_baseball_dir(const unsigned  int& baseballDir) {baseball_dir = baseballDir;}
-//    [[nodiscard]] unsigned int get_baseball_dir() const {return baseball_dir;}
     void HandleMove(const int& x_border);
 
 private:
